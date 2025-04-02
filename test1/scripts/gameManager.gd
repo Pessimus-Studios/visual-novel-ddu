52c2724fda584595265ef93b2c277f1a38a4ac39
MAGNUS. W. HINGE <122803385+MacBeef@users.noreply.github.com>
2025-04-02 23:11:44 +0200

    GameManager finished.
    - Fixed dialogic/GameManager interplay
    - Fixed dialoigic timeline not playing
    - Added mental instability

--- test1/scripts/gameManager.gd
+++ test1/scripts/gameManager.gd
@@ -1,6 +1,11 @@
 extends Node  # Changed from Control to Node for a singleton
 var gameStage: String = "start"
 
+func _ready() -> void:
+	handle_scene_change()
+	
+	pass
+
 func change_game_stage(new_stage: String) -> void:
 	gameStage = new_stage
 	handle_scene_change()
@@ -12,8 +17,6 @@ func handle_scene_change() -> void:
 		"cutScene1":
 			get_tree().change_scene_to_file("res://scenes/cutScene1.tscn")
 		"beginning":
-			Dialogic.start_timeline("res://timelines/mainTimeline.dtl")
-func _ready() -> void:
-	handle_scene_change()
-	pass
+			Dialogic.start("res://timelines/mainTimeline.dtl")
+
 	
