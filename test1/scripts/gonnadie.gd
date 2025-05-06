1fadbb038a1ceeee45932db407aca5e771c757ac
cheese705 <143507434+cheese705@users.noreply.github.com>
2025-05-06 13:58:59 +0200

    1.55 STORY ADDED (1/3)
    added story.

---
+++ test1/scripts/gonnadie.gd
@@ -0,0 +1,5 @@
+extends Control
+# Code for the second cutscene. 
+func _on_video_stream_player_finished() -> void:
+	GameManager.change_game_stage("Conflict2_timeline") # Cues the next stage once video is finished.
+	pass # Replace with function body.
