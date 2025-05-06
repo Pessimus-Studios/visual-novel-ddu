1d3b10e91890245e949cc26b7abfb63198801822
MAGNUS. W. HINGE <122803385+MacBeef@users.noreply.github.com>
2025-05-06 08:22:21 +0200

    1.41 update general

--- test1/scripts/gameManager.gd
+++ test1/scripts/gameManager.gd
@@ -3,10 +3,24 @@ extends Node
 var gameStage: String = "start"
 var saveSlot: String
 var currentMood: int = 10
+var desaturation_overlay: CanvasLayer
+var overlay_scene = preload("res://scenes/saturatrionDrain.tscn")
 
 func _ready() -> void:
 	handle_scene_change()
-	pass
+	# Preload but don't instantiate yet
+	preload("res://scenes/saturatrionDrain.tscn")
+
+func enable_desaturation(intensity: float = 1.0) -> void:
+	if not desaturation_overlay:
+		var overlay_scene = preload("res://scenes/saturatrionDrain.tscn")
+		desaturation_overlay = overlay_scene.instantiate()
+		get_tree().root.add_child(desaturation_overlay)
+		# Move it to the top (in case other CanvasLayers exist)
+		desaturation_overlay.layer = 128  # High number to ensure it's on top
+	
+	var color_rect = desaturation_overlay.get_node("ColorRect")
+	color_rect.material.set_shader_parameter("intensity", intensity)
 
 # Function that changes the gameStage to the string it recieves and updates the game stage.
 # Call the function by: GameManager.change_game_stage("")
@@ -16,10 +30,13 @@ func change_game_stage(new_stage: String) -> void:
 
 func game_save_slot(newSave: String) -> void:
 	saveSlot = newSave
+
+func save_Game() -> void:
+	Dialogic.Save.save(saveSlot)
 	
 func choice_mood(choiceMood: int) -> void:
 	currentMood += choiceMood
- 
+
 func handle_scene_change() -> void: 
 	# Switch statement for changing game stage
 	match GameManager.gameStage:
--- test1/scripts/startscreen.gd
+++ test1/scripts/startscreen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready() -> void:
-	hide_buttons([$SaveSlot1, $deleteSlot1, $SaveSlot2, $deleteSlot2, $back])
+	hide_buttons([$SaveSlot1, $deleteSlot1, $SaveSlot2, $deleteSlot2, $back, $fullscreen])
 	
 # Hides buttons when called
 func hide_buttons(buttons: Array):
@@ -14,32 +14,46 @@ func show_buttons(buttons: Array):
 	for button in buttons:
 		button.show()
 		button.disabled = false
-		
+
+# Script for the quit button on the startscreen.
+func _on_quit_button_down() -> void:
+	get_tree().quit()
+
+# Script for universal back button
+func _on_back_button_down() -> void:
+	hide_buttons([$SaveSlot1, $deleteSlot1, $SaveSlot2, $deleteSlot2, $fullscreen, $back])
+	show_buttons([$Start, $Settings, $Quit])
+
+
+##############################
+#   Start Panel stuff Below  #
+##############################
+# Script for the setting button on the startscreen.
+func _on_settings_button_down() -> void:
+	show_buttons([$fullscreen, $back])
+	hide_buttons([$Start, $Settings, $Quit])
+	
+# Script to turn fullscreen if u are in windowed, and into windowed if u are in fullscreen
+func _on_fullscreen_button_down() -> void:
+	var current_mode = DisplayServer.window_get_mode()
+	if current_mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+##############################
+#   Start Panel stuff Below  #
+##############################
+
 func _on_start_button_down() -> void:
 	# Only shows the delete button for saves if they have data on.
 	if Dialogic.Save.has_slot("1"):
 		show_buttons([$deleteSlot1])
 	if Dialogic.Save.has_slot("2"):
 		show_buttons([$deleteSlot2])
-	
 	# Shows the save slots and the back buttons, while hiding the start, settings and quit buttons
 	show_buttons([$SaveSlot1, $SaveSlot2, $back])
 	hide_buttons([$Start, $Settings, $Quit])
-	pass
-	
-# Script for the setting button on the startscreen.
-func _on_settings_button_down() -> void:
-	pass 
-
-# Script for the quit button on the startscreen.
-func _on_quit_button_down() -> void:
-	get_tree().quit()
-
-# Start Panel stuff Below
-func _on_back_button_down() -> void:
-	hide_buttons([$SaveSlot1, $deleteSlot1, $SaveSlot2, $deleteSlot2, $back])
-	show_buttons([$Start, $Settings, $Quit])
-	pass
 
 # Code for save button 1
 func _on_save_slot_1_button_down() -> void:
@@ -61,11 +75,12 @@ func _on_save_slot_2_button_down() -> void:
 		
 # Code for deleting save slot 1
 func _on_delete_slot_1_button_down() -> void:
-	Dialogic.Save.delete_slot("1") 
+	Dialogic.Save.delete_slot("1")
+	$SaveSlot1.text = $SaveSlot1.text
 	hide_buttons([$deleteSlot1])
-	pass 
 
 # Code for deleting save slot 2
 func _on_delete_slot_2_button_down() -> void:
 	Dialogic.Save.delete_slot("2")
+	$SaveSlot2.text = $SaveSlot2.text
 	hide_buttons([$deleteSlot2])
